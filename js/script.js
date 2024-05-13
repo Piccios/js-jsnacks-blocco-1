824424087c805ae9a8a6251caa201d8d7ee8b489
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-13 11:51:37 +0200

    created two prompt in wich user will add two numbers; console will print the bigger number

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore. 
+
+let numero1 = prompt('Inserisci un numero');
+
+let numero2 = prompt('inserisci un altro numero');
+
+if( numero1 > numero2) {
+    console.log(numero1);
+    }
+else ( console.log(numero2));
