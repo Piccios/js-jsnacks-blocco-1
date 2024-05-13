2f93dae110ea09d61ede040476e053c17247ca18
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-13 12:41:07 +0200

    now the request is asked 6 times

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -2,12 +2,13 @@
 
 // Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero. se è dispari inseriscilo nell'array
 
-
-const numbers = [];
-
+for (let i = 0; i < 6; i++) {
+    const numbers = [];
 let userNumber = Number.parseInt(prompt("Inserisci un numero: "));
 
 if (userNumber % 2 === 1) { 
     numbers.push(userNumber);
     console.log(numbers);
 }
+}
+
