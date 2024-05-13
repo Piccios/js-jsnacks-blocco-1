ae74f240f21783d1b3f8c69ab9e14f9cbdcd4f7a
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-13 12:32:21 +0200

    corrected math

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -7,6 +7,7 @@ const numbers = [];
 
 let userNumber = Number.parseInt(prompt("Inserisci un numero: "));
 
-if (userNumber % 2 === 0) { 
+if (userNumber % 2 === 1) { 
     numbers.push(userNumber);
+    console.log(numbers);
 }
