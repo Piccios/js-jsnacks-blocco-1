43774af3d473feb2ee562a1c3c1304df50aafb9e
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-14 11:45:07 +0200

    creati due array con i numeri del primo array generato:  uno con i numeri pari e uno con i numeri dispari

--- snack-8/Js/script.js
+++ snack-8/Js/script.js
@@ -1,7 +1,7 @@
 
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
 
-//i multipli di 5 verranno stampari con un testo giallo 
+// poi i multipli di 5 verranno stampari con un testo giallo 
 
 const array = [3, 5, 7, 3, 4, 2, 1, 6, 10, 18, 21, 15]
 
@@ -9,9 +9,20 @@ const array = [3, 5, 7, 3, 4, 2, 1, 6, 10, 18, 21, 15]
 
 let divVerde = document.createElement('div');
 
+
 let divRosso = document.createElement('div');
 
-//
+//stampo in divVerde i numeri dispari e in divRosso i numeri pari presenti in array 
+
+if (array.length > 0) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            divVerde.innerHTML += array[i];
+        } else {
+            divRosso.innerHTML += array[i];
+        }
+    }
+}
 
 // inserisco i due div nell'html
 
