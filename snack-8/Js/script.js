1f53ebfdf201d3dcf90fcffa905d1ade5d9d84bb
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-14 11:30:32 +0200

    new snack folder

---
+++ snack-8/Js/script.js
@@ -0,0 +1,4 @@
+Snack8 
+// Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
+
+//i multipli di 5 verranno stampari con un testo giallo 
