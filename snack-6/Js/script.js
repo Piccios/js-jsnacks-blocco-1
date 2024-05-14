874caef003d8d2418db16b350729f0268fc629ed
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-14 10:53:42 +0200

    correction

--- snack-6/Js/script.js
+++ snack-6/Js/script.js
@@ -3,3 +3,12 @@
 
 let numbers = [1, 6, 5, 3, 4, 8, 13, 18, 19, 31]
 
+let somma = 0;
+
+for (let index = 0; index < numbers.length; index++) {
+    if (index % 2!== 0) {
+        somma += numbers[index]
+    }
+}
+
+console.log( numbers + ' ' + somma)
